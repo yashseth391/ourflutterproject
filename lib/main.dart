4728a2473c60296e679178ef15834a3bd0511aa4
yashseth391 <115554024+yashseth391@users.noreply.github.com>
2024-03-24 23:47:41 +0530

    last stateless widget

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:currency_converter/currency_convertor_material.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: CurrencyConvertorMaterialPage(),
+    );
+  }
+}
