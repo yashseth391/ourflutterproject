00219615b2fdeef12e61714231543c432e714e9a
yashseth391 <115554024+yashseth391@users.noreply.github.com>
2024-03-25 23:56:10 +0530

    first statefull work

--- lib/currency_convertor_material.dart
+++ lib/currency_convertor_material.dart
@@ -7,18 +7,16 @@ class CurrencyConvertorMaterialPagee extends StatefulWidget {
 
   @override
   State<CurrencyConvertorMaterialPagee> createState() {
-    print('create state');
-    _ return _CurrecnyConvertorMaterialPagee() ;
+    return _CurrecnyConvertorMaterialPagee();
   }
 }
 
 class _CurrecnyConvertorMaterialPagee
     extends State<CurrencyConvertorMaterialPagee> {
+  final TextEditingController textEditingController = TextEditingController();
+  double result = 0;
   @override
   Widget build(BuildContext context) {
-    print('build fn');
-    double result = 0;
-    final TextEditingController textEditingController = TextEditingController();
     const border = OutlineInputBorder(
       borderSide: BorderSide(
         width: 2.0,
@@ -81,93 +79,9 @@ class _CurrecnyConvertorMaterialPagee
               padding: const EdgeInsets.all(10.0),
               child: TextButton(
                 onPressed: () {
-                  result = double.parse(textEditingController.text) * 81;
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.black,
-                  foregroundColor: const Color.fromARGB(255, 255, 255, 255),
-                  minimumSize: const Size(double.infinity, 50),
-                ),
-                child: const Text("CONVERT"),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class CurrencyConvertorMaterialPage extends StatelessWidget {
-  const CurrencyConvertorMaterialPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    double result = 0;
-    final TextEditingController textEditingController = TextEditingController();
-    const border = OutlineInputBorder(
-      borderSide: BorderSide(
-        width: 2.0,
-        style: BorderStyle.solid,
-        strokeAlign: BorderSide.strokeAlignOutside,
-      ),
-      borderRadius: BorderRadius.all(
-        Radius.circular(90),
-      ),
-    );
-    return Scaffold(
-      backgroundColor: const Color.fromRGBO(122, 167, 152, 1),
-      appBar: AppBar(
-        backgroundColor: const Color.fromRGBO(122, 167, 152, 1),
-        elevation: 0,
-        title: const Text(
-          'currecny convertor',
-          style: TextStyle(
-            color: Color.fromARGB(255, 0, 0, 0),
-          ),
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              result.toString(),
-              style: const TextStyle(
-                fontSize: 45,
-                fontWeight: FontWeight.bold,
-                color: Color.fromRGBO(0, 0, 0, 1),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: textEditingController,
-                style: const TextStyle(
-                  color: Color.fromARGB(255, 0, 0, 0),
-                  fontWeight: FontWeight.bold,
-                ),
-                decoration: const InputDecoration(
-                  hintText: 'Please enter currency in rupees',
-                  hintStyle: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black),
-                  prefixIcon: Icon(Icons.monetization_on_outlined),
-                  prefixIconColor: Colors.black,
-                  fillColor: Colors.white,
-                  filled: true,
-                  focusedBorder: border,
-                  enabledBorder: border,
-                ),
-                keyboardType: const TextInputType.numberWithOptions(),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: TextButton(
-                onPressed: () {
-                  result = double.parse(textEditingController.text) * 81;
+                  setState(() {
+                    result = double.parse(textEditingController.text) * 81;
+                  });
                 },
                 style: TextButton.styleFrom(
                   backgroundColor: Colors.black,
