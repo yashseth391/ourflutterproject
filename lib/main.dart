518eefce5d0a29b3db51fff846041fecc4d4c8c7
yashseth391 <115554024+yashseth391@users.noreply.github.com>
2024-04-20 14:12:40 +0530

    change to flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,237 @@
-import 'package:currency_converter/currency_convertor_material.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: CurrencyConvertorMaterialPagee(),
+    return MaterialApp(
+      title: 'Login/Signup Page',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/login': (context) => LoginPage(),
+        '/signup': (context) => SignupPage(),
+      },
     );
   }
 }
 
-class MycupertinoApp extends StatelessWidget {
-  const MycupertinoApp({super.key});
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Home Page',
+              style: TextStyle(fontSize: 24.0),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/login');
+              },
+              child: Text('Login'),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/signup');
+              },
+              child: Text('Sign Up'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image:
+                AssetImage('images/bg2.jpg'), // Path to your background image
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 20.0),
+                const TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    labelText: 'Username',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    // Add login logic here
+                  },
+                  child: const Text('Login'),
+                ),
+                const SizedBox(height: 20.0),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignupPage()),
+                    );
+                  },
+                  child: const Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignupPage extends StatelessWidget {
+  const SignupPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
-      home: CurrencyConvertorMaterialPagee(),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Signup',
+          style: TextStyle(color: Colors.black), // Text color
+        ),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/bg.jpg'), // Path to your image asset
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'Signup Page',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    color: Colors.black, // Text color
+                    fontWeight: FontWeight.bold, // Bold font weight
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                TextField(
+                  style: const TextStyle(
+                    color: Colors.white, // Text color of the input field
+                    fontWeight: FontWeight.bold, // Bold font weight
+                  ),
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white, // Background color
+                    labelText: 'Username',
+                    labelStyle: const TextStyle(
+                      color: Colors.black, // Text color of the label
+                      fontWeight: FontWeight.bold, // Bold font weight
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none, // No border
+                      borderRadius:
+                          BorderRadius.circular(10.0), // Rounded corners
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                TextField(
+                  style: const TextStyle(
+                    color: Colors.white, // Text color of the input field
+                    fontWeight: FontWeight.bold, // Bold font weight
+                  ),
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white, // Background color
+                    labelText: 'Password',
+                    labelStyle: const TextStyle(
+                      color: Colors.black, // Text color of the label
+                      fontWeight: FontWeight.bold, // Bold font weight
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none, // No border
+                      borderRadius:
+                          BorderRadius.circular(10.0), // Rounded corners
+                    ),
+                  ),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    // Add signup logic here
+                  },
+                  child: const Text('Signup'),
+                ),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    // Navigate back to login page
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Back to Login'),
+                ),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    // Navigate back to home page
+                    Navigator.popUntil(context, ModalRoute.withName('/'));
+                  },
+                  child: const Text('Back to Home'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
